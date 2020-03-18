8a714c2002eaaf0d2b967ceea5f1719f3b4ddcc5
osingcoder <58870909+osingcoder@users.noreply.github.com>
2020-03-18 16:25:49 +0700

    variabel dan tipe data

---
+++ variabel dan tipe data.py
@@ -0,0 +1,35 @@
+# membuat variabel
+nama_variabel = "nilai"
+variabel1 = "ini nilai dai variabelnya"
+print(variabel1)
+
+# menghapus variabel
+nama = "osing coder"
+print(nama)
+
+print(50*'=')
+
+#Tipe Data
+# data angka:
+umur = 19 #ini tipe integer
+berat = 59.5 #ini tipe float
+print(type(umur))
+print(type(berat))
+# tipe data teks:
+karakter = 'R' #ini tipe char
+teks = "ini tipe string"
+print(teks)
+print(type(teks))
+
+print(50*'=')
+#tipe data boolean
+malas = False
+ngoding = True
+print(type(malas))
+
+#cara casting/mengubah tipe data
+# misal:
+angka1 = 5
+angka2 = 8
+hasil = float(angka1)+angka2
+print(hasil, type(hasil))
